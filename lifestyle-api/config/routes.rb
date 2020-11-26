62619f2281581fc9038bb72c400ffecf2e4c46b3
David Ruiz <davidl-ruizc@outlook.com>
2020-11-25 21:05:30 -0500

    routes

--- lifestyle-api/config/routes.rb
+++ lifestyle-api/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :products, only: [:index, :show]
+
+    resource :user, only: [:create]
+
+    resource :session, only: [:create, :destroy]
+  end
 end
